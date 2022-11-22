79bc7e2f5cc85a433bc4d2d83ef1e9ea9b7f2a6d
Moritz <96340080+mol42069@users.noreply.github.com>
2022-11-22 14:54:58 +0100

    .
    multiple empty recs doesent quite work yet

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import pygame as py
 import random as rnd
+import sys
 
 py.init()
 screen_size = (1920, 1080)
 
+# importing all the necessary images
+
 start_img = py.image.load('images/starting_img.png')
 marked_img = py.image.load('images/marked_img.png')
 bomb_img = py.image.load('images/bomb_img.png')
+zero_img = py.image.load('images/0_img.png')
+one_img = py.image.load('images/1_img.png')
+two_img = py.image.load('images/2_img.png')
+three_img = py.image.load('images/3_img.png')
+four_img = py.image.load('images/4_img.png')
+five_img = py.image.load('images/5_img.png')
+six_img = py.image.load('images/6_img.png')
+seven_img = py.image.load('images/7_img.png')
+eight_img = py.image.load('images/8_img.png')
 
 
 class Rectangle:
 
-    def __init__(self, size, neighbors, x, y, starting_img):
-        self.starting_img = starting_img
+    def __init__(self, size, neighbors, x, y):
+        self.starting_img = start_img
+        self.marked_img = marked_img
+        self.revealed_img = images_io[0]
         self.size = size
         self.x = x
         self.y = y
         self.neighbors = neighbors
         self.mine = False
         self.flag = False
+        self.revealed = False
         self.num = 0
         self.rect = py.Rect(x, y, size, size)
 
@@ -26,18 +41,76 @@ class Rectangle:
 
         display.blit(self.starting_img, (self.rect.x, self.rect.y))
 
+    def click(self):
+        global left_clicked, right_clicked, pos
 
-def init_display_rec(n_graph, starting_img, x=64, y=36, rec_size=30):
-    rectangles = []
+        # checking if mouse is on the rectangle
+
+        if self.rect.collidepoint(pos):
+
+            # here we check for the left mouse button:
+
+            if py.mouse.get_pressed()[0] == 1 and not left_clicked:
+                left_clicked = True
+                self.l_click()
+                return True
+
+            elif py.mouse.get_pressed()[0] != 1:
+                left_clicked = False
+
+            # here we check for the right mouse button:
+
+            if py.mouse.get_pressed()[2] == 1 and not right_clicked:
+                right_clicked = True
+                if not self.flag and not self.revealed:
+                    # here we draw the flag on this rectangle
+                    self.flag = True
+                    display.blit(self.marked_img, (self.rect.x, self.rect.y))
+                elif not self.revealed:
+                    self.flag = False
+
+                    # here we draw the starting img on this rectangle
+                    display.blit(self.starting_img, (self.rect.x, self.rect.y))
+            elif py.mouse.get_pressed()[2] != 1:
+                right_clicked = False
+
+        return False
+
+    def l_click(self):
+        if not self.flag:
+            if not self.revealed:
+                # we check if this rectangle is already revealed and then if it is a mine
+                if self.mine:
+                    # TODO: game over state here
+                    pass
+                else:
+                    display.blit(self.revealed_img, (self.rect.x, self.rect.y))
+                    self.revealed = True
+
+
+def check_if_ne(iy, ix):
+    global rectangles, graph, visited
+
+    for n in graph[iy, ix]:
+        print(n)
+        if n not in visited:
+            visited.append(n)
+            rectangles[n[0]][n[1]].l_click()
+            if rectangles[n[0]][n[1]].num == 0:
+                check_if_ne(n[0], n[1])
+
+
+def init_display_rec(n_graph, x=63, y=36, rec_size=30):
+    t_rectangles = []
     for yy in range(0, y):
         rectangles_x = []
         for xx in range(0, x):
             neighbors = n_graph[(yy, xx)]
-            rectangles_x.append(Rectangle(rec_size, neighbors, xx * 30, yy * 30, starting_img))
+            rectangles_x.append(Rectangle(rec_size, neighbors, xx * 30, yy * 30))
             rectangles_x[xx].init()
-        rectangles.append(rectangles_x)
+        t_rectangles.append(rectangles_x)
     py.display.flip()
-    return rectangles
+    return t_rectangles
 
 
 def generate_neighbors():                                   # generates a dictionary which tells us what the neighbors
@@ -50,7 +123,7 @@ def generate_neighbors():                                   # generates a dictio
                     if x == 0:
                         n_graph.update({(y, x): [(y, x + 1), (y + 1, x), (y + 1, x + 1)]})
 
-                    elif x == 63:
+                    elif x == 64:
                         n_graph.update({(y, x): [(y + 1, x), (y, x - 1), (y + 1, x - 1)]})
 
                     else:
@@ -59,17 +132,17 @@ def generate_neighbors():                                   # generates a dictio
                     if x == 0:
                         n_graph.update({(y, x): [(y - 1, x), (y, x + 1), (y - 1, x + 1)]})
 
-                    elif x == 63:
+                    elif x == 64:
                         n_graph.update({(y, x): [(y - 1, x), (y, x - 1), (y - 1, x - 1)]})
 
                     else:
                         n_graph.update({(y, x): [(y - 1, x), (y, x + 1), (y, x - 1), (y - 1, x - 1), (y - 1, x + 1)]})
 
-            elif x == 0 or x == 63:
+            elif x == 0 or x == 64:
                 if x == 0:
-                    n_graph.update({(y, x): [(y + 1, x), (y - 1, x), (y, x + 1), (y + 1, x + 1), (y + 1, x - 1)]})
+                    n_graph.update({(y, x): [(y + 1, x), (y - 1, x), (y, x + 1), (y + 1, x + 1), (y + 1, x)]})
 
-                if x == 63:
+                if x == 64:
                     n_graph.update({(y, x): [(y + 1, x), (y - 1, x), (y, x - 1), (y + 1, x - 1), (y - 1, x + 1)]})
 
             else:
@@ -79,48 +152,68 @@ def generate_neighbors():                                   # generates a dictio
     return n_graph
 
 
-def generate_bombs(c_difficulty, c_rectangles):
+def generate_bombs():
+    global rectangles, difficulty
 
-    for iy, yy in enumerate(c_rectangles):
+    for iy, yy in enumerate(rectangles):
         for ix, xx in enumerate(yy):
-            if rnd.randint(0, c_difficulty * 10):
+            if rnd.randint(0, difficulty * 2) == 1:
                 rectangles[iy][ix].mine = True
             else:
                 pass
-    return rectangles
 
 
-def give_numbers(t_graph, t_rectangles):
-    for iy, yy in enumerate(t_rectangles):
-        for ix, xx in enumerate(t_rectangles):
-            for z in t_graph[(iy, ix)]:
-                if t_rectangles[z[0]][z[1]].mine:
-                    t_rectangles[iy][ix].num += 1
+def give_numbers():
+    global graph, rectangles
+    for iy, yy in enumerate(rectangles):
+        for ix, xx in enumerate(rectangles):
+            for z in graph[(iy, ix)]:
+                if rectangles[z[0]][z[1]].mine:
+                    rectangles[iy][ix].num += 1
+            rectangles[iy][ix].revealed_img = images_io[rectangles[iy][ix].num]
 
-    return t_rectangles
 
+def check_rec():
+    global rectangles, graph, pos
+    for iy, yy in enumerate(rectangles):
+        for ix, xx in enumerate(yy):
+            if xx.click():
+                if rectangles[iy][ix].num == 0:
+                    check_if_ne(iy, ix)
+
+
+sys.setrecursionlimit(3000)
+
+# images in list in order:
+images_io = [zero_img, one_img, two_img, three_img, four_img, five_img, six_img, seven_img, eight_img]
 
 display = py.display.set_mode(screen_size, py.FULLSCREEN)
 running = True
+left_clicked = False
+right_clicked = False
 # generate the dictionary for the neighbors
 
 graph = generate_neighbors()
 
 # initialize the rectangles in a pattern
-rectangles = init_display_rec(graph, start_img, 64, 36, 30)
+rectangles = init_display_rec(graph, 63, 36, 30)
 
 difficulty = 5          # the higher, the more bombs there are if 0 there are none
 
 # init the bombs
 
-rectangles = generate_bombs(difficulty, rectangles)
+generate_bombs()
 
 # init the number of bombs around the rectangle
 
-rectangles = give_numbers(graph, rectangles)
-
+give_numbers()
 
 while running:
+    visited = []
+    # we get the position of the mouse.
+    pos = py.mouse.get_pos()
+    check_rec()
+    py.display.flip()
 
     for event in py.event.get():
         if event.type == py.QUIT:
